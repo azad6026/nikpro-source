473f06cc06aeb653abce41d8887c7d4a0910ae5d
azadeh <azad6026@gmail.com>
2019-08-22 16:03:33 +1000

    Added home link on top

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -20,6 +20,14 @@ class BlogPostTemplate extends React.Component {
         />
         <article>
           <header>
+       <nav>
+          <ul>
+            <li>
+                <Link to="/" >
+                </Link>
+            </li>
+          </ul>
+        </nav>
             <h1
               style={{
                 marginTop: rhythm(1),
@@ -30,7 +38,7 @@ class BlogPostTemplate extends React.Component {
             </h1>
             <p
               style={{
-                ...scale(-1 / 5),
+                ...scale(-1 / 4),
                 display: `block`,
                 marginBottom: rhythm(1),
               }}
@@ -45,7 +53,6 @@ class BlogPostTemplate extends React.Component {
             }}
           />
           <footer>
-            <Bio />
           </footer>
         </article>
 
